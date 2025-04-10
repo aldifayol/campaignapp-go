c3d04dd4a3c4311c21656e016011b4562c64dbe2
Aldi <aldifayol@gmail.com>
2025-04-10 19:58:59 +0800

    post users route implementation

--- main.go
+++ main.go
@@ -19,6 +19,21 @@ var users = []user{
 	{ ID: "2", FullName: "Lopez Hanna", PhoneNumber: "08114321234", Email: "lopez@gamil.com", IsAdmin: true},
 }
 
+// postUsers adds a user from JSON received in the request body.
+func postUsers(c *gin.Context) {
+    var newUser user
+
+    // Call BindJSON to bind the received JSON to
+    // newUser.
+    if err := c.BindJSON(&newUser); err != nil {
+        return
+    }
+
+    // Add the new album to the slice.
+    users = append(users, newUser)
+    c.IndentedJSON(http.StatusCreated, newUser)
+}
+
 func getUsers(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, users)
 }
@@ -26,6 +41,7 @@ func getUsers(c *gin.Context) {
 func main() {
     router := gin.Default()
     router.GET("/users", getUsers)
+		router.POST("/users", postUsers)
 
     router.Run("localhost:8080")
 }
